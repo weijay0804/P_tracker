70dca73381ba810d26eea0a52c4e2937283d4b98
andy <andy_sam@gmail.com>
2023-06-06 03:02:27 +0800

    refactor: 將 record 路由更名為 records

--- app/main/views.py
+++ app/main/views.py
@@ -2,12 +2,13 @@
 Author: andy
 Date: 2023-06-06 01:48:42
 LastEditors: andy
-LastEditTime: 2023-06-06 02:28:18
+LastEditTime: 2023-06-06 03:00:02
 Description: app 主試圖
 '''
 
 
 from flask import render_template, request
+from flask_login import login_required, current_user
 
 from . import main
 
@@ -17,12 +18,10 @@ def index():
     return render_template("main/index.html")
 
 
-@main.route("/record", methods=["GET", "POST"])
-def record():
-    """記帳路由"""
-
-    if request.method == "POST":
-        pass
+@main.route("/record", methods=["GET"])
+@login_required
+def records():
+    """顯示所有記帳路由"""
 
     record = [{"name": "test1", "price": 120, "date": "2022-01-01", "desc": "this is a test"}]
-    return render_template("main/record.html", record=record)
+    return render_template("main/records.html", record=record)
