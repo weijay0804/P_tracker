b517d5e0b96e3e6b72a514bd7bb5357982b33f91
andy <andy_sam@gmail.com>
2023-06-06 01:00:16 +0800

    build: 初始化 資料庫遷移系統

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,16 @@
 Author: andy
 Date: 2023-06-06 00:17:42
 LastEditors: andy
-LastEditTime: 2023-06-06 00:22:05
+LastEditTime: 2023-06-06 00:48:41
 Description: 初始化 app
 '''
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
+
+db = SQLAlchemy()
 
 
 def create_app() -> "Flask":
@@ -17,6 +20,8 @@ def create_app() -> "Flask":
     app = Flask(__name__)
     app.config.from_object(Config)
     Config.init_app(app)
+
+    db.init_app(app)
 
     @app.route("/")
     def index():
--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 Author: andy
 Date: 2023-06-06 00:22:26
 LastEditors: andy
-LastEditTime: 2023-06-06 00:23:38
+LastEditTime: 2023-06-06 00:58:27
 Description: app 運行主程式
 '''
 
+from flask_migrate import Migrate
 
-from app import create_app
+from app import create_app, db
 
 app = create_app()
+
+migrate = Migrate(app, db)
