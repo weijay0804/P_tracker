702a615d3a8b4c50ce2c933b34ac0dc5f5155753
andy <andy_sam@gmail.com>
2023-06-06 00:57:26 +0800

    feat: 更改 資料庫路徑

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 Author: andy
 Date: 2023-06-06 00:19:25
 LastEditors: andy
-LastEditTime: 2023-06-06 00:22:14
+LastEditTime: 2023-06-06 00:38:48
 Description: app 設定檔
 '''
 
@@ -17,7 +17,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASEDIR, "database.db")
 
     @staticmethod
     def init_app(app):
