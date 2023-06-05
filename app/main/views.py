db4d67d9444e73f0fbd480ce4a2f4ecc214d1e8d
andy <andy_sam@gmail.com>
2023-06-06 02:37:25 +0800

    feat: 新增 記帳紀錄頁面

--- app/main/views.py
+++ app/main/views.py
@@ -2,12 +2,12 @@
 Author: andy
 Date: 2023-06-06 01:48:42
 LastEditors: andy
-LastEditTime: 2023-06-06 02:09:00
+LastEditTime: 2023-06-06 02:28:18
 Description: app 主試圖
 '''
 
 
-from flask import render_template
+from flask import render_template, request
 
 from . import main
 
@@ -15,3 +15,14 @@ from . import main
 @main.route("/")
 def index():
     return render_template("main/index.html")
+
+
+@main.route("/record", methods=["GET", "POST"])
+def record():
+    """記帳路由"""
+
+    if request.method == "POST":
+        pass
+
+    record = [{"name": "test1", "price": 120, "date": "2022-01-01", "desc": "this is a test"}]
+    return render_template("main/record.html", record=record)
