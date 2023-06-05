64e1c0cb0f51fad4a7d5327e71e904e23c59fa1d
andy <andy_sam@gmail.com>
2023-06-06 03:30:10 +0800

    feat: 新增 新增記帳功能

--- app/main/views.py
+++ app/main/views.py
@@ -2,14 +2,17 @@
 Author: andy
 Date: 2023-06-06 01:48:42
 LastEditors: andy
-LastEditTime: 2023-06-06 03:00:02
+LastEditTime: 2023-06-06 03:29:39
 Description: app 主試圖
 '''
 
+from datetime import datetime
 
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 
+from app import db
+from app.db_model import Record
 from . import main
 
 
@@ -23,5 +26,36 @@ def index():
 def records():
     """顯示所有記帳路由"""
 
-    record = [{"name": "test1", "price": 120, "date": "2022-01-01", "desc": "this is a test"}]
-    return render_template("main/records.html", record=record)
+    user = current_user
+
+    records = user.records
+
+    return render_template("main/records.html", records=records)
+
+
+@main.route("/add_record", methods=["GET", "POST"])
+@login_required
+def add_record():
+    """新增記帳路由"""
+
+    user = current_user
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        price = request.form.get("price")
+        desc = request.form.get("desc")
+        date = request.form.get("date")
+
+        date_object = datetime.strptime(date, "%Y-%m-%d").date()
+
+        record = Record(name=name, price=price, desc=desc, date=date_object)
+
+        user.records.append(record)
+
+        db.session.add(record)
+        db.session.commit()
+
+        flash("新增成功")
+        return redirect(url_for("main.records"))
+
+    return render_template("main/add_record.html")
